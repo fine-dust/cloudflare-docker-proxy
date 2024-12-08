ec6e0d6efe278f9eb70087a26edd41f146dabb4a
Vito <hczhch@ymail.com>
2024-12-08 17:55:30 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ async function handleRequest(request) {
     );
   }
   // return docs
-  if (url.hostname.startsWith('docker.') && url.pathname === "/") {
+  if (url.pathname === "/") {
     return new Response(DOCS, {
       status: 200,
       headers: {
